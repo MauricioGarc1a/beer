68d264a3c98b2a1f4bcad16934b8cf370e0534f0
khalilgharbaoui <kaygeee@gmail.com>
2016-02-04 16:30:11 +0100

    corrected the beer.rb person.rb and bar.rb files

--- bar.rb
+++ bar.rb
@@ -3,19 +3,17 @@ require './person'
 require './bartender'
 require './customer'
 
-
-captain_jack = Customer.new("Cpt. Jack")
 the_bartender = Bartender.new("Joe")
+captain_jack = Customer.new("Cpt. Jack")
 
-​
 the_bartender.greet(captain_jack)
 the_bartender.serve(captain_jack)
-​
+
 # Captain Jack can drink while he has beer
 while captain_jack.has_beer?
-  captain_jack.drink!
-  sleep 1
+  captain_jack.drink!
+  sleep 1
 end
-​
+
 captain_jack.drink!
 the_bartender.serve(captain_jack)
--- person.rb
+++ person.rb
@@ -2,22 +2,21 @@ class Person
   def initialize(name)
     @name = name
   end
-​
+
   def greet(other_person)
     say "Hi, how are ya?"
     other_person.greet_back
   end
-​
+
   def greet_back
     say "Fine"
   end
-​
+
   def what_is_your_name
     say @name
   end
-​
+
   def say(message)
     puts "#{@name}: #{message}!"
   end
 end
-#this is the person file
