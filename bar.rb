d5f07f2264c2d3759dab1a4cd26b43e9aebb89f4
MaurcioGarc1a <maoricio07@gmail.com>
2016-02-04 15:26:34 +0100

    welcome to bar.rb

---
+++ bar.rb
@@ -0,0 +1,14 @@
+the_bartender = Bartender.new("Joe")
+captain_jack = Customer.new("Cpt. Jack")
+​
+the_bartender.greet(captain_jack)
+the_bartender.serve(captain_jack)
+​
+# Captain Jack can drink while he has beer
+while captain_jack.has_beer?
+  captain_jack.drink!
+  sleep 1
+end
+​
+captain_jack.drink!
+the_bartender.serve(captain_jack)
