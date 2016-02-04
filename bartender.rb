305f881aed738c7f2a9e91be2e87d364d8a1ff3b
khalilgharbaoui <kaygeee@gmail.com>
2016-02-04 15:20:04 +0100

    file with bartender class

---
+++ bartender.rb
@@ -0,0 +1,16 @@
+class Bartender < Person
+  def initialize(name)
+    super # Call the initialize(name) method in the super class (Person)
+    @customers = []
+  end
+​
+  def greet(customer)
+    super
+    @customers << customer
+  end
+​
+  def serve(customer)
+    say "There ya go.."
+    customer.take_beer(Beer.new)
+  end
+end
